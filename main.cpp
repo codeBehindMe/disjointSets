6503644f2e7ae3e517655dfc50fd81fb0fc44e3d
codeBehindMe <aaron.tillekeratne@simplyenergy.com.au>
2017-08-18 16:33:14 +1000

    updated sets class

--- main.cpp
+++ main.cpp
@@ -1,169 +1,152 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-
-class Maze {
-private:
-    vector<vector<int>> _cells;
-
-    void preProcess() {
-        // Go through the maze and assign a cell its own set.
-        unsigned _cellCount = 0;
-        for (int i = 0; i < this->_cells.size(); i++) {
-            for (int j = 0; j < this->_cells[i].size(); j++) {
-                this->_cells[i][j] = _cellCount;
-                _cellCount++;
-            }
-        }
-    }
-
-public:
-    Maze(unsigned rows, unsigned columns) {
-        this->_cells = vector<vector<int>>(rows);
-
-        for (int i = 0; i < rows; i++) {
-            this->_cells[i].resize(columns);
-        }
-        // Call to preprocess the maze.
-        preProcess();
-    }
-
-    void printMaze() {
-        for (int i = 0; i < this->_cells.size(); i++) {
-            for (int j = 0; j < this->_cells[i].size(); j++) {
-                cout << this->_cells[i][j] << ' ';
-            }
-            cout << endl;
-        }
-    }
-
+struct Rank {
+    int parent;
+    int rank;
 };
 
-class DisjointSet {
-
+class Set {
 public:
-    void makeSet(int x) {
-        // Makes a singleton set.
-    };
+    explicit Set(int size);
 
-    int Find(int x) {
-        // Returns the id of the set containing x.
-        // Satisfies property :
-        // If x & y lie in the same set, then Find(x) == Find(y)
-    };
+    int Find(int i);
 
-    void Union(int x, int y) {
-        // Merges two sets containing x and y.
-    }
-};
+    void Union(int i, int j);
 
-
-// Using arrays with smallest element
-class SmallestSet {
-private:
-    vector<unsigned> _set;
-public:
-    explicit SmallestSet(unsigned size) {
-        // Creates a universe of size S objects with each with their own set
-
-        // Create the empty vector
-        _set = vector<unsigned>(size);
-
-        // Assign the smallest to their own i;
-        for (unsigned i = 0; i < this->_set.size(); i++) {
-            makeSet(i);
-        }
-
-    }
-
-    void makeSet(unsigned i) {
-        // Makes a set
-        this->_set[i] = i;
-    }
-
-    unsigned setFind(unsigned i) {
-        return this->_set[i];
-    }
-
-    void unionItems(unsigned i, unsigned j) {
-
-        // Get the smallest set number of i and j.
-        unsigned i_id = setFind(i);
-        unsigned j_id = setFind(j);
-
-        // If they are equal, there is no more to union, so just return.
-        if (i_id == j_id) {
-            return;
-        }
-
-        // if not, get the smaller of the two ids.
-        unsigned _min = min(i_id, j_id);
-
-        // Scan the array from 1:n and then assign the smallest of the id (_min) to all items that belonged to i_id or j_id set.
-        for (unsigned int &k : this->_set) {
-            if (k == i_id || k == j_id) {
-                k = _min;
-            }
-        }
-    }
-
-    void printSet() {
-        for (auto &k : this->_set) {
-            cout << k << ' ';
-        }
-        cout << endl;
-    }
-
-};
-
-// Using trees
-class TreeSet {
+    void PrintSet();
 
 private:
-    vector<unsigned> _parent;
-public:
-    explicit TreeSet(unsigned size) {
-        // Generate the universe.
-        _parent = vector<unsigned>(size);
+    vector<Rank> _elements;
 
-        // Make each item in its own set (set parent to itself)
-        for (unsigned i = 0; i < size; i++) {
-            makeSet(i);
-        }
-    }
+    int _find(int i);
 
-    inline void makeSet(unsigned i) {
-        // Make the item its own parent.
-        this->_parent[i] = i;
-    }
+    void _union(int i, int j);
 
-    unsigned findSet(unsigned i){
-        // Find by looking for the root.
-        while(i != _parent[i]){
-            i = _parent[i];
-        }
-        return i;
-    }
+    void _makeSet(int i);
 
 };
+
+Set::Set(int size) {
+    this->_elements = vector<Rank>(size);
+
+    for (unsigned i = 0; i < _elements.size(); i++) {
+        _makeSet(i);
+    }
+}
+
+void Set::_makeSet(int i) {
+    // Makeset.
+    this->_elements[i].parent = i;
+    this->_elements[i].rank = 0; // Set default rank to 0
+}
+
+int Set::_find(int i) {
+    // Find the root of an element.
+    while (this->_elements[i].parent != i) {
+        i = this->_elements[i].parent;
+    }
+    return i;
+}
+
+void Set::_union(int i, int j) {
+    // Find the roots of i & j
+    int root_i = _find(i);
+    int root_j = _find(j);
+
+    // Check if they are already joined.
+    if (root_i == root_j) return;
+
+    // Check the rank before assigning
+    if (_elements[root_i].rank > _elements[root_j].rank) {
+        _elements[root_j].parent = root_i;
+    } else if (_elements[root_i].rank < _elements[root_j].rank) {
+        _elements[root_i].parent = root_j;
+    } else {
+        // If the ranks are equal, we need to increment the rank.
+        _elements[root_i].parent = root_j;
+        _elements[root_j].rank++;
+    }
+}
+
+int Set::Find(int i) {
+    // Public method takes indexing from 1.
+    int i_z = i - 1;
+    int r_z = _find(i_z);
+    return r_z + 1;
+}
+
+void Set::Union(int i, int j) {
+    // Public method takes indexing from 1.
+    int i_z = i - 1;
+    int j_z = j - 1;
+    _union(i_z, j_z);
+}
+
+void Set::PrintSet() {
+    cout << setw(12) << setfill(' ') << "ELEMENT :";
+    for (int i = 0; i < this->_elements.size(); i++) {
+        cout << setw(2) << setfill(' ') << i + 1 << '|';
+    }
+    cout << endl;
+    cout << setw(12) << setfill(' ') << "PARENT :";
+    for (int i = 0; i < this->_elements.size(); i++) {
+        cout << setw(2) << setfill(' ') << _elements[i].parent + 1 << '|';
+    }
+    cout << endl;
+    cout << setw(12) << setfill(' ') << "TREE DEPTH :";
+    for (int i = 0; i < this->_elements.size(); i++) {
+        cout << setw(2) << setfill(' ') << _elements[i].rank << '|';
+    }
+    cout << endl;
+}
+
 
 int main() {
 
-//    Maze myMaze = Maze(10, 10);
-//
-//    myMaze.printMaze();
+    Set mySet = Set(12);
+    mySet.Union(2, 10);
+    mySet.Union(7, 5);
+    mySet.Union(6, 1);
+    mySet.Union(3, 4);
+    mySet.Union(5, 11);
+    mySet.Union(7, 8);
+    mySet.Union(7, 3);
+    mySet.Union(12, 2);
+    mySet.Union(9, 6);
 
-    SmallestSet smallestSet(10);
+    cout << mySet.Find(6) << endl;
+    cout << mySet.Find(3) << endl;
+    cout << mySet.Find(11) << endl;
+    cout << mySet.Find(9) << endl;
 
-    smallestSet.unionItems(3, 5);
+    mySet.PrintSet();
 
-    smallestSet.unionItems(5, 7);
+    Set aSet = Set(20);
+    for(int i =1; i < 20; i++){
+        aSet.Union(i,i+1);
+    }
 
-    smallestSet.printSet();
+    aSet.PrintSet();
+
+    Set bigSet = Set(60);
+
+    for (int i = 1; i <= 30; i++) {
+        bigSet.Union(i, 2 * i);
+    }
+    for (int i = 1; i <= 20; i++) {
+        bigSet.Union(i, 3 * i);
+    }
+    for (int i = 1; i <= 12; i++) {
+        bigSet.Union(i, 5 * i);
+    }
+
+    bigSet.PrintSet();
+
     char p;
-
     cin >> p;
     return 0;
 }
